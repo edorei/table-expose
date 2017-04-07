6aeda1d7230cb8acde9344b9980db241a4d7444e
Vinod <vinod.sharma@olamnet.com>
2017-04-07 11:18:01 +0530

    first commit

---
+++ expose/src/main/java/com/edorei/expose/App.java
@@ -0,0 +1,13 @@
+package com.edorei.expose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
