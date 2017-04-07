7574c6afdb9e6a6e57a1ea825f6a27c1c5e4de8b
Vinod <vinod.sharma@olamnet.com>
2017-04-07 14:53:47 +0530

    persestence xm

--- expose-test/src/main/java/com/edorei/expose/test/App.java
+++ expose-test/src/main/java/com/edorei/expose/test/App.java
@@ -1,13 +1,29 @@
 package com.edorei.expose.test;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import com.edorei.expose.test.model.Country;
+
+public class App {
+    public static void main( String[] args ) {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("edoreiJPA");
+    	EntityManager entityManager = emf.createEntityManager();
+    	
+    	entityManager.getTransaction().begin();
+    	
+    	TypedQuery<Country> query = entityManager.createQuery("from Country",Country.class);
+    	List<Country> countries = query.getResultList();
+    	for (Country country : countries) {
+			System.out.println(country);
+		}
+    	
+    	entityManager.getTransaction().commit();
+    	entityManager.close();
+    	emf.close();
     }
 }
---
+++ expose-test/src/main/java/com/edorei/expose/test/model/Country.java
@@ -0,0 +1,89 @@
+package com.edorei.expose.test.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="country")
+public class Country implements Serializable{
+
+	private static final long serialVersionUID = -5331359018396341523L;
+	
+	@Id
+	@Column(name = "country_id")
+	private int countryId;
+	
+	@Column(name = "country")
+	private String country;
+	
+	@Column(name = "last_update")
+	private Date lastUpdate;
+
+	public int getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(int countryId) {
+		this.countryId = countryId;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + countryId;
+		result = prime * result + ((lastUpdate == null) ? 0 : lastUpdate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Country other = (Country) obj;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (countryId != other.countryId)
+			return false;
+		if (lastUpdate == null) {
+			if (other.lastUpdate != null)
+				return false;
+		} else if (!lastUpdate.equals(other.lastUpdate))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Country [countryId=" + countryId + ", country=" + country + ", lastUpdate=" + lastUpdate + "]";
+	}
+}
